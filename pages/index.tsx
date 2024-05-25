1884394de51933889cc459a012e91045922168bd
delcode <delcode92@gmail.com>
2024-05-25 10:36:21 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 const WebSocketPage = () => {
   const [message, setMessage] = useState('');
-  const [receivedMessages, setReceivedMessages] = useState([]);
+  const [receivedMessages, setReceivedMessages] = useState<(string | ArrayBuffer)[]>([]);
+  const client = new W3CWebSocket('ws://localhost:8080');
 
   useEffect(() => {
-    const client = new W3CWebSocket('ws://localhost:8080');
 
     client.onopen = () => {
       console.log('WebSocket Client Connected');
@@ -14,12 +14,12 @@ const WebSocketPage = () => {
 
     client.onmessage = (message) => {
       console.log('Received message:', message.data);
-      setReceivedMessages(prevMessages => [...prevMessages, message.data]);
+      setReceivedMessages(prevMessages => [...prevMessages, message.data as string | ArrayBuffer]);
     };
 
-    return () => {
-      client.close();
-    };
+    // return () => {
+    //   // client.close();
+    // };
   }, []);
 
   const handleSend = () => {
@@ -40,8 +40,8 @@ const WebSocketPage = () => {
       <div>
         <h2>Received Messages</h2>
         <ul>
-          {receivedMessages.map((msg, index) => (
-            <li key={index}>{msg}</li>
+          {receivedMessages.map((mesg, index) => (
+            <li key={index}>{mesg.toString()}</li>
           ))}
         </ul>
       </div>
